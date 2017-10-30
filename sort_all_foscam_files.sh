7ac7c0affe29400c4129f30e043e10fe2fc51090
Bart Verheyde <leopard.solutions@gmail.com>
2017-10-30 16:27:17 +0100

    Synology do all camera fodlers

---
+++ sort_all_foscam_files.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Camera Gang
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D5C/snap/ /volume1/foscam_beelden/FI9853EP_00626E6D4D5C/snap_sorted/ --move
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D5C/record/ /volume1/foscam_beelden/FI9853EP_00626E6D4D5C/record_sorted/ --move
+# Camera Voorkant
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D67/snap/ /volume1/foscam_beelden/FI9853EP_00626E6D4D67/snap_sorted/ --move
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D67/record/ /volume1/foscam_beelden/FI9853EP_00626E6D4D67/record_sorted/ --move
+# Camera Zijkant
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D75/snap/ /volume1/foscam_beelden/FI9853EP_00626E6D4D75/snap_sorted/ --move
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D75/record/ /volume1/foscam_beelden/FI9853EP_00626E6D4D75/record_sorted/ --move
+# Camera Garages
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D78/snap/ /volume1/foscam_beelden/FI9853EP_00626E6D4D78/snap_sorted/ --move
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D78/record/ /volume1/foscam_beelden/FI9853EP_00626E6D4D78/record_sorted/ --move
+# Camera Trimster
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D7B/snap/ /volume1/foscam_beelden/FI9853EP_00626E6D4D7B/snap_sorted/ --move
+python3 /volume1/foscam_beelden/sort_script/phockup.py /volume1/foscam_beelden/FI9853EP_00626E6D4D7B/record/ /volume1/foscam_beelden/FI9853EP_00626E6D4D7B/record_sorted/ --move
+
+
